59d9141976283b4db9fecee266e930c2055ed8fc
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-06-24 23:39:07 +0600

    added navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,50 @@
 import React from "react";
+import {
+  SearchOutlined,
+  LanguageOutlined,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  NotificationsNoneOutlined,
+  ChatBubbleOutlineOutlined,
+  ListOutlined,
+} from "@mui/icons-material";
 import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className="sidebar">
-      <div className="top">Logo</div>
-      <div className="center">List</div>
-      <div className="bottom">Color option</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlined />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlined className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlined className="icon" />
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="avatar" className="avatar" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
